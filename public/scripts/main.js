f44bd3d9bf4bda172346bf5bb9f51ce43f6621f8
josephmchristy <josephmchristy@gmail.com>
2018-11-17 22:54:16 -0800

    styled home page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,17 +1,17 @@
 var imageNo = 1;
 
 setInterval(() => {
-	$("#content .image-" + imageNo).removeClass("fadeIn");
-	$("#content .image-" + imageNo).addClass("fadeOut");
+	$("#images .image-" + imageNo).removeClass("fadeIn");
+	$("#images .image-" + imageNo).addClass("fadeOut");
 	setTimeout(() => {
-		$("#content .image-" + imageNo).removeClass("animated fadeOut");
-		$("#content .image-" + imageNo).addClass("hidden");
+		$("#images .image-" + imageNo).removeClass("animated fadeOut");
+		$("#images .image-" + imageNo).addClass("hidden");
 		imageNo++;
 		if(imageNo > 4){
 			imageNo = 1;
 		}
-		$("#content .image-" + imageNo).removeClass("hidden");
-		$("#content .image-" + imageNo).addClass("animated fadeIn");
+		$("#images .image-" + imageNo).removeClass("hidden");
+		$("#images .image-" + imageNo).addClass("animated fadeIn");
 	}, 1000);
 
-}, 3 * 1000)
+}, 6 * 1000)
